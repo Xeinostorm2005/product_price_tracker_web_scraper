adb2cf366507a8dfe7bc3efbfc6e6e8a4e9ca5ac
Xeinostorm2005 <231753761+Xeinostorm2005@users.noreply.github.com>
2025-11-12 23:26:12 +0100

    Added comments to the code

--- src/utils/custom_errors.py
+++ src/utils/custom_errors.py
@@ -5,10 +5,12 @@ from typing import Literal
 # Creates a custom error exception for .env file
 class MissingVariablesInEnv(Exception):
 
+    # Default variables
     default_message_notfound = "Found missing variables in the .env file!"
     default_message_wrong_type = "Wrong value types in the .env file!"
     default_error_code = 500
 
+    # Initialize Error Exception
     def __init__(
         self,
         error_type: Literal["notfound", "wrong_type"],
@@ -16,10 +18,16 @@ class MissingVariablesInEnv(Exception):
         message: str | None = None,
         code: int | None = None,
     ):
+        # Respond with a specific message
         if error_type == "notfound":
             self.message = message or self.default_message_notfound
         else:
             self.message = message or self.default_message_wrong_type
+
+        # Returns default or specific error code
         self.code = code or self.default_error_code
+
+        # Shows which variables needs to be updated
         self.variables = variables
+
         super().__init__(self.message)
