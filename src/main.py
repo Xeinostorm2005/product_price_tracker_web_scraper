15fa3f033a2ecd12c0ad89c63bb3afafcb28258a
Xeinostorm2005 <231753761+Xeinostorm2005@users.noreply.github.com>
2025-11-13 13:23:58 +0100

    Testing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
+from src.utils.env_check import check_env
+from src.utils.logger import log
+from dotenv import load_dotenv
+from src.utils.error_handler import handle_errors
 
+# Loads .env file
+load_dotenv()
+
+
+@handle_errors(re_raise=False, default=None)
 def execute():
-    pass
+
+    # Check if app is ready to run
+    check_env()
+
+    log.info("main.py", "All variables in .env file have been checked")
+    log.debug("main.py", "All variables in .env file have been checked")
+    log.success("main.py", "All variables in .env file have been checked")
+    log.warn("main.py", "All variables in .env file have been checked")
+    log.error("main.py", "All variables in .env file have been checked")
+    log.critical("main.py", "All variables in .env file have been checked")
